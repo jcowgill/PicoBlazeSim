02166887b16f0802c90d2dee1a79dc5f1388dbce
James Cowgill <james410@cowgill.org.uk>
2013-02-20 16:46:16 +0000

    Rewritten Processor class to use a builder rather than a constructor

--- Processor.cs
+++ Processor.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using System;
+
 namespace JCowgill.PicoBlazeSim
 {
     /// <summary>
@@ -6,72 +7,109 @@ namespace JCowgill.PicoBlazeSim
     /// </summary>
     public class Processor
     {
-        public static readonly Processor PicoBlazeCpld  = new Processor( 256,  8,  4, 0, 0);
-        public static readonly Processor PicoBlaze      = new Processor( 256, 16, 15, 0, 0);
-        public static readonly Processor PicoBlazeII    = new Processor(1024, 32, 31, 0, 0);
-        public static readonly Processor PicoBlaze3 =
-            new Processor(1024, 16, 31, 64, ProcessorFlags.HasStoreFetch |
-                                            ProcessorFlags.HasTestCompare);
+        #region Builtin Processors
+
+        public static readonly Processor PicoBlazeCpld = new Builder()
+        {
+            RomSize = 256,
+            InterruptVector = 0xFF,
+            RegisterCount = 8,
+            StackSize = 4,
+
+        }.Build();
+
+        public static readonly Processor PicoBlaze = new Builder()
+        {
+            RomSize = 256,
+            InterruptVector = 0xFF,
+            RegisterCount = 16,
+            StackSize = 15,
+
+        }.Build();
+
+        public static readonly Processor PicoBlazeII = new Builder()
+        {
+            RomSize = 256,
+            InterruptVector = 0xFF,
+            RegisterCount = 32,
+            StackSize = 31,
+
+        }.Build();
+
+        public static readonly Processor PicoBlaze3 = new Builder()
+        {
+            RomSize = 1024,
+            InterruptVector = 0x3FF,
+            RegisterCount = 16,
+            StackSize = 31,
+            ScratchpadSize = 64,
+            Flags = ProcessorFlags.HasStoreFetch | ProcessorFlags.HasTestCompare,
+
+        }.Build();
+
+        #endregion
 
         /// <summary>
         /// Gets the size of the instruction ROM
         /// </summary>
-        public int RomSize
-        {
-            get;
-            private set;
-        }
+        public short RomSize { get; private set; }
+
+        /// <summary>
+        /// The address to jump to when an interrupt occurs
+        /// </summary>
+        public short InterruptVector { get; private set; }
 
         /// <summary>
         /// Gets the number of registers
         /// </summary>
-        public int RegisterCount
-        {
-            get;
-            private set;
-        }
+        public byte RegisterCount { get; private set; }
 
         /// <summary>
         /// Gets the size of the call stack
         /// </summary>
-        public int StackSize
-        {
-            get;
-            private set;
-        }
+        public int StackSize { get; private set; }
 
         /// <summary>
         /// Gets the size of scratchpad ram
         /// </summary>
-        public int ScratchpadSize
-        {
-            get;
-            private set;
-        }
+        public byte ScratchpadSize { get; private set; }
 
         /// <summary>
         /// Gets the flags for this processor
         /// </summary>
-        public ProcessorFlags Flags
+        public ProcessorFlags Flags { get; private set; }
+
+        /// <summary>
+        /// Creates a new processor from a builder
+        /// </summary>
+        /// <param name="builder">builder to create from</param>
+        public Processor(Builder builder)
         {
-            get;
-            private set;
+            // Copy properties over
+            this.RomSize            = builder.RomSize;
+            this.InterruptVector    = builder.InterruptVector;
+            this.RegisterCount      = builder.RegisterCount;
+            this.StackSize          = builder.StackSize;
+            this.ScratchpadSize     = builder.ScratchpadSize;
+            this.Flags              = builder.Flags;
         }
 
         /// <summary>
-        /// Creates a new ProcessorOptions class
+        /// Helper class for building processors
         /// </summary>
-        public Processor(int romSize,
-                                int registerCount,
-                                int stackSize,
-                                int scratchpadSize,
-                                ProcessorFlags flags)
+        public class Builder
         {
-            this.RomSize = romSize;
-            this.RegisterCount = registerCount;
-            this.StackSize = stackSize;
-            this.ScratchpadSize = scratchpadSize;
-            this.Flags = flags;
+            public short RomSize { get; set; }
+            public short InterruptVector { get; set; }
+            public byte RegisterCount { get; set; }
+            public int StackSize { get; set; }
+            public byte ScratchpadSize { get; set; }
+            public ProcessorFlags Flags { get; set; }
+
+            public Processor Build()
+            {
+                return new Processor(this);
+            }
         }
     }
 }
